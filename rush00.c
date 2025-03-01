d0ac568bf66cb91f6d2b019057e37ace3b3ebc4f
Jaeden Ting Yiyong <126431591+JaedenTYY@users.noreply.github.com>
2025-03-01 18:10:08 +0800

    Rush00

---
+++ rush00.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jating <jating@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 16:04:30 by jating            #+#    #+#             */
+/*   Updated: 2025/03/01 16:34:59 by jating           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print(int row, char first, char between, char last)
+{
+	int	i;
+
+	i = 1;
+	while (i <= row)
+	{
+		if (i == 1)
+			ft_putchar(first);
+		else if (i == row)
+			ft_putchar(last);
+		else
+			ft_putchar(between);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	row;
+	int	column;
+
+	i = 1;
+	row = x;
+	column = y;
+	if (row >= 1 && column >= 1)
+	{
+		while (i <= column)
+		{
+			if (i == 1)
+				ft_print(row, 'o', '-', 'o');
+			else if (i == column)
+				ft_print(row, 'o', '-', 'o');
+			else
+				ft_print(row, '|', ' ', '|');
+			i++;
+		}
+	}
+}
---
+++ rush04.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jating <jating@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 17:41:17 by jating            #+#    #+#             */
+/*   Updated: 2025/03/01 18:00:59 by jating           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print(int row, char first, char between, char last)
+{
+	int	i;
+
+	i = 1;
+	while (i <= row)
+	{
+		if (i == 1)
+			ft_putchar(first);
+		else if (i == row)
+			ft_putchar(last);
+		else
+			ft_putchar(between);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	row;
+	int	column;
+
+	i = 1;
+	row = x;
+	column = y;
+	if (row >= 1 && column >= 1)
+	{
+		while (i <= column)
+		{
+			if (i == 1)
+				ft_print(row, 'A', 'B', 'C');
+			else if (i == column)
+				ft_print(row, 'C', 'B', 'A');
+			else
+				ft_print(row, 'B', ' ', 'B');
+			i++;
+		}
+	}
+}
