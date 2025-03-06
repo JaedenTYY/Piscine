f4bbedc172ec7f7d511b282d9a641f2eec237867
“JaedenTYY” <tingjaeden@gmail.comgit config --global user.name JaedenTYYgit config --global user.email tingjaeden@gmail.com>
2025-03-06 10:44:52 +0800

    ex01/

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -2,15 +2,20 @@
 
 char *ft_strncpy(char *dest, char *src, unsigned int n)
 {
+    // Declare a variable to store the index
     unsigned int i;
 
     i = 0;
+
+    // Copy the source string to the destination string
     while (src[i] != '\0' && i < n)
     {
+        // Copy the character from the source string to the destination string
         dest[i] = src[i];
         i++;
     }
 
+    // Add null characters to the end of the destination string
     while (i < n)
     {
         dest[i] = '\0';
@@ -21,9 +26,9 @@ char *ft_strncpy(char *dest, char *src, unsigned int n)
 
 int main(void)
 {
-    char src[] = "Source";
-    char dest[] = "######";
-    int n = 6;
+    char src[] = "Source"; // The source string
+    char dest[] = "######"; // The destination string
+    int n = 6; // The number of characters to copy
 
     printf("Before calling function: %s\n", dest);
     ft_strncpy(dest, src, n);
